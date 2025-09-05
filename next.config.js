3e7b5787501e5789fb98f30e29be9f3b8f571c28
unknown <sohaibab21@gmail.com>
2025-09-05 10:40:25 +0200

    mon-05-09-2025.v1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  experimental: {}, // remove old appDir flag
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 's.wordpress.com' }, // mShots
